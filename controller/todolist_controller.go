e2a5ca057579e078611a76286115409d47e85f41
Syahda Romansyah <syahdarom@gmail.com>
2023-02-13 10:45:13 +0700

    feat: create todolist controller interface

---
+++ controller/todolist_controller.go
@@ -0,0 +1,14 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type TodolistController interface {
+	Create(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
+	Update(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
+	Delete(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
+	FindAll(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
+}
