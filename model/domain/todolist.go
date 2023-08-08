5a7aa94198e7a28a27b574955606d252f45091fa
Syahda Romansyah <syahdarom@gmail.com>
2023-08-08 15:21:12 +0700

    Revert "feat: create a todolist DB scheme"
    This reverts commit 2d29c5736031f28debfb601af221d3fa66b7575f.

--- model/domain/todolist.go
+++ model/domain/todolist.go
@@ -1,10 +1,10 @@
 package domain
 
 type Todolist struct {
-	Id              string   `json:"id"`
-	Done            bool     `json:"done"`
-	Tags            []string `json:"tags"`
-	TodolistMessage string   `json:"todolistMessage"`
-	CreatedAt       string   `json:"createdAt"`
-	UpdatedAt       string   `json:"updatedAt"`
+	Id              string
+	Done            bool
+	Tags            []string
+	TodolistMessage string
+	CreatedAt       string
+	UpdatedAt       string
 }
--- model/scheme/todolist_db.go
+++
@@ -1,8 +0,0 @@
-package scheme
-
-import "github.com/syahdaromansyah/pg1-todolist-restful-api-go-json/model/domain"
-
-type TodolistDB struct {
-	Todolists []domain.Todolist `json:"todolists"`
-	Total     uint              `json:"total"`
-}
