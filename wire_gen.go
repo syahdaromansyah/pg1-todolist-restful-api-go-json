fdbed6e2a46e119c2c73b29aa147b6cfdc6bbd81
Syahda Romansyah <syahdarom@gmail.com>
2023-08-18 16:43:09 +0700

    feat: update the dependencies injector using Google Wire

--- wire_gen.go
+++ wire_gen.go
@@ -8,6 +8,7 @@ package main
 
 import (
 	"github.com/go-playground/validator/v10"
+	"github.com/google/wire"
 	"github.com/syahdaromansyah/pg1-todolist-restful-api-go-json/app"
 	"github.com/syahdaromansyah/pg1-todolist-restful-api-go-json/controller"
 	"github.com/syahdaromansyah/pg1-todolist-restful-api-go-json/repository"
@@ -18,11 +19,19 @@ import (
 // Injectors from injector.go:
 
 func InitializeServer(dbPath string) *http.Server {
-	todolistRepository := repository.NewTodolistRepositoryImpl()
+	todolistRepositoryImpl := repository.NewTodolistRepositoryImpl()
 	validate := validator.New()
-	todolistService := service.NewTodolistServiceImpl(todolistRepository, dbPath, validate)
-	todolistController := controller.NewTodolistControllerImpl(todolistService)
-	router := app.NewRouter(todolistController)
+	todolistServiceImpl := service.NewTodolistServiceImpl(todolistRepositoryImpl, dbPath, validate)
+	todolistControllerImpl := controller.NewTodolistControllerImpl(todolistServiceImpl)
+	router := app.NewRouter(todolistControllerImpl)
 	server := app.NewServer(router)
 	return server
 }
+
+// injector.go:
+
+var todolistRepoSet = wire.NewSet(repository.NewTodolistRepositoryImpl, wire.Bind(new(repository.TodolistRepository), new(*repository.TodolistRepositoryImpl)))
+
+var todolistServiceSet = wire.NewSet(service.NewTodolistServiceImpl, wire.Bind(new(service.TodolistService), new(*service.TodolistServiceImpl)))
+
+var todolistControllerSet = wire.NewSet(controller.NewTodolistControllerImpl, wire.Bind(new(controller.TodolistController), new(*controller.TodolistControllerImpl)))
