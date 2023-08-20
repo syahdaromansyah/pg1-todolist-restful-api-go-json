12fb51954c828730e716543aabd3b10e36ed0d7c
Syahda Romansyah <syahdarom@gmail.com>
2023-08-20 14:11:26 +0700

    feat: update a WriteLogToFile helper function
    Implement a Logrus package for logging purposes

--- helper/write_log_to_file.go
+++ helper/write_log_to_file.go
@@ -1,15 +1,20 @@
 package helper
 
 import (
-	"log"
 	"os"
+
+	"github.com/sirupsen/logrus"
 )
 
+var Logger = logrus.New()
+
 func WriteLogToFile(logFn func()) {
+	Logger.SetFormatter(&logrus.JSONFormatter{})
+
 	file, _ := os.OpenFile("./log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 
 	defer file.Close()
 
-	log.SetOutput(file)
+	Logger.SetOutput(file)
 	logFn()
 }
