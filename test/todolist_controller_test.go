ca819d65281f125fec7765412471ec1f91d544f5
Syahda Romansyah <syahdarom@gmail.com>
2023-08-23 13:09:17 +0700

    test: update todolist controller test
    Update the TestCreateTodolistSuccess function test

--- test/todolist_controller_test.go
+++ test/todolist_controller_test.go
@@ -32,81 +32,170 @@ func resetTodolistsDB() {
 	helper.DoPanicIfError(err)
 }
 
+func readTodolistDB() *scheme.TodolistDB {
+	todolistJsonBytes, err := os.ReadFile(dbPath)
+	helper.DoPanicIfError(err)
+
+	todolistDB := &scheme.TodolistDB{}
+	err = json.Unmarshal(todolistJsonBytes, todolistDB)
+	helper.DoPanicIfError(err)
+
+	return todolistDB
+}
+
 func TestCreateTodolistSuccess(t *testing.T) {
+	type todolistReqTest struct {
+		TodolistReq       web.TodolistCreateRequest
+		ExpectedTotalTags int
+	}
+
 	tableTests := []struct {
-		Data            web.TodolistCreateRequest
-		ExpectedTagsLen int
+		TodolistReqsTest      []todolistReqTest
+		ExpectedTotalTodolist uint
 	}{
 		{
-			Data: web.TodolistCreateRequest{
-				Tags:            []string{},
-				TodolistMessage: "This is a Todo",
+			TodolistReqsTest: []todolistReqTest{
+				{
+					TodolistReq: web.TodolistCreateRequest{
+						Tags:            []string{},
+						TodolistMessage: "This is a Todo",
+					},
+					ExpectedTotalTags: 0,
+				},
 			},
-			ExpectedTagsLen: 0,
+			ExpectedTotalTodolist: 1,
 		},
 		{
-			Data: web.TodolistCreateRequest{
-				Tags:            []string{"Programming"},
-				TodolistMessage: "Learn Go Unit Test",
+			TodolistReqsTest: []todolistReqTest{
+				{
+					TodolistReq: web.TodolistCreateRequest{
+						Tags:            []string{"Programming"},
+						TodolistMessage: "Learn Go Unit Test",
+					},
+					ExpectedTotalTags: 1,
+				},
 			},
-			ExpectedTagsLen: 1,
+			ExpectedTotalTodolist: 1,
 		},
-
 		{
-			Data: web.TodolistCreateRequest{Tags: []string{"Cloud", "Virtual Machine"},
-				TodolistMessage: "Learn Virtual Machine in Cloud"},
-
-			ExpectedTagsLen: 2,
-		},
-
-		{
-			Data: web.TodolistCreateRequest{
-				Tags:            []string{"Programming", "OOP", "Async"},
-				TodolistMessage: "Learn Java Async",
+			TodolistReqsTest: []todolistReqTest{
+				{
+					TodolistReq: web.TodolistCreateRequest{
+						Tags:            []string{"Cloud", "Virtual Machine"},
+						TodolistMessage: "Learn Virtual Machine in Cloud",
+					},
+					ExpectedTotalTags: 2,
+				},
 			},
-			ExpectedTagsLen: 3,
+			ExpectedTotalTodolist: 1,
+		},
+		{
+			TodolistReqsTest: []todolistReqTest{
+				{
+					TodolistReq: web.TodolistCreateRequest{
+						Tags:            []string{"Programming", "OOP", "Async"},
+						TodolistMessage: "Learn Java Async",
+					},
+					ExpectedTotalTags: 3,
+				},
+			},
+			ExpectedTotalTodolist: 1,
+		},
+		{
+			TodolistReqsTest: []todolistReqTest{
+				{
+					TodolistReq: web.TodolistCreateRequest{
+						Tags:            []string{},
+						TodolistMessage: "This is a Todo",
+					},
+					ExpectedTotalTags: 0,
+				},
+				{
+					TodolistReq: web.TodolistCreateRequest{
+						Tags:            []string{"Programming"},
+						TodolistMessage: "Learn Go Unit Test",
+					},
+					ExpectedTotalTags: 1,
+				},
+				{
+					TodolistReq: web.TodolistCreateRequest{
+						Tags:            []string{"Cloud", "Virtual Machine"},
+						TodolistMessage: "Learn Virtual Machine in Cloud",
+					},
+					ExpectedTotalTags: 2,
+				},
+				{
+					TodolistReq: web.TodolistCreateRequest{
+						Tags:            []string{"Programming", "OOP", "Async"},
+						TodolistMessage: "Learn Java Async",
+					},
+					ExpectedTotalTags: 3,
+				},
+			},
+			ExpectedTotalTodolist: 4,
 		},
 	}
 
 	for _, test := range tableTests {
-		t.Run(test.Data.TodolistMessage, func(t *testing.T) {
-			router := setupRouterTest()
-			payload := &web.TodolistCreateRequest{
-				Tags:            test.Data.Tags,
-				TodolistMessage: test.Data.TodolistMessage,
-			}
+		todolistsRes := []domain.Todolist{}
 
-			payloadBytes, err := json.Marshal(payload)
-			helper.DoPanicIfError(err)
+		for _, todolistReqTest := range test.TodolistReqsTest {
+			todolistReqTags := todolistReqTest.TodolistReq.Tags
+			todolistReqMessage := todolistReqTest.TodolistReq.TodolistMessage
 
-			reqBody := strings.NewReader(string(payloadBytes))
-			httpReq := httptest.NewRequest(http.MethodPost, todolistsPath, reqBody)
-			httpReq.Header.Add("Content-Type", "application/json")
+			t.Run(todolistReqMessage, func(t *testing.T) {
+				router := setupRouterTest()
+				payload := &web.TodolistCreateRequest{
+					Tags:            todolistReqTags,
+					TodolistMessage: todolistReqMessage,
+				}
 
-			recorder := httptest.NewRecorder()
-			router.ServeHTTP(recorder, httpReq)
+				payloadBytes, err := json.Marshal(payload)
+				helper.DoPanicIfError(err)
 
-			response := recorder.Result()
-			assert.Equal(t, 201, response.StatusCode)
+				reqBody := strings.NewReader(string(payloadBytes))
+				httpReq := httptest.NewRequest(http.MethodPost, todolistsPath, reqBody)
+				httpReq.Header.Add("Content-Type", "application/json")
 
-			resBodyBytes, err := io.ReadAll(response.Body)
-			helper.DoPanicIfError(err)
+				recorder := httptest.NewRecorder()
+				router.ServeHTTP(recorder, httpReq)
 
-			resBody := &web.WebResponse[domain.Todolist]{}
+				response := recorder.Result()
+				assert.Equal(t, 201, response.StatusCode)
 
-			err = json.Unmarshal(resBodyBytes, resBody)
-			helper.DoPanicIfError(err)
+				resBodyBytes, err := io.ReadAll(response.Body)
+				helper.DoPanicIfError(err)
 
-			assert.Equal(t, 201, resBody.Code)
-			assert.Equal(t, "success", resBody.Status)
-			assert.Equal(t, test.ExpectedTagsLen, len(resBody.Data.Tags))
-			assert.Equal(t, test.Data.TodolistMessage, resBody.Data.TodolistMessage)
-			assert.Equal(t, resBody.Data.UpdatedAt, resBody.Data.CreatedAt)
-			assert.NotEqual(t, "", resBody.Data.Id)
-			assert.False(t, resBody.Data.Done)
+				resBody := &web.WebResponse[domain.Todolist]{}
 
-			resetTodolistsDB()
-		})
+				err = json.Unmarshal(resBodyBytes, resBody)
+				helper.DoPanicIfError(err)
+
+				todolistsRes = append(todolistsRes, domain.Todolist{
+					Id:              resBody.Data.Id,
+					Done:            resBody.Data.Done,
+					Tags:            resBody.Data.Tags,
+					TodolistMessage: resBody.Data.TodolistMessage,
+					CreatedAt:       resBody.Data.CreatedAt,
+					UpdatedAt:       resBody.Data.UpdatedAt,
+				})
+
+				assert.Equal(t, 201, resBody.Code)
+				assert.Equal(t, "success", resBody.Status)
+				assert.Equal(t, todolistReqTest.ExpectedTotalTags, len(resBody.Data.Tags))
+				assert.Equal(t, todolistReqMessage, resBody.Data.TodolistMessage)
+				assert.Equal(t, resBody.Data.UpdatedAt, resBody.Data.CreatedAt)
+				assert.NotEmpty(t, resBody.Data.Id)
+				assert.False(t, resBody.Data.Done)
+			})
+		}
+
+		todolistDB := readTodolistDB()
+
+		assert.ElementsMatch(t, todolistDB.Todolists, todolistsRes)
+		assert.Equal(t, test.ExpectedTotalTodolist, todolistDB.Total)
+
+		resetTodolistsDB()
 	}
 }
 
