9f95fc3c0e14612f8da31886c924eb54b7bcad8b
Syahda Romansyah <syahdarom@gmail.com>
2023-08-26 06:08:38 +0700

    feat: update the todolist repository interface
    Update the interface method of 'Update' and 'Delete' return type

--- repository/todolist_repository.go
+++ repository/todolist_repository.go
@@ -4,8 +4,8 @@ import "github.com/syahdaromansyah/pg1-todolist-restful-api-go-json/model/domain
 
 type TodolistRepository interface {
 	Save(dbPath string, todolistRequest domain.Todolist) domain.Todolist
-	Update(dbPath string, todolistRequest domain.Todolist) domain.Todolist
-	Delete(dbPath string, todolistRequest domain.Todolist)
+	Update(dbPath string, todolistRequest domain.Todolist) (domain.Todolist, error)
+	Delete(dbPath string, todolistIdParam string) error
 	FindById(dbPath string, todolistIdParam string) (domain.Todolist, error)
 	FindAll(dbPath string) []domain.Todolist
 }
